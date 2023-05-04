70d8b929faaba7bb47ba6e54b21a87ea36c836a3
anton <anton@mail.ru>
2023-05-04 19:52:22 +0300

    uye4gerg

--- frontend/src/ApolloClient.tsx
+++ frontend/src/ApolloClient.tsx
@@ -4,7 +4,7 @@ import apolloLogger from 'apollo-link-logger';
 import { getTokens, saveTokens } from "./manage-tokens";
 
 function backendApiUrl() {
-  return 'https://diplom-full-stack-v8-hochetsa-plakat-jv1v-kvxahmqhc-rikishi1742.vercel.app/graphql';
+  return 'https://diplom-full-stack-v8-hochetsa-plakat-g4x9.vercel.app/graphql';
 }
 
 const BACKEND_API_URL = backendApiUrl();
@@ -57,7 +57,7 @@ const authMiddleware = new ApolloLink((operation, forward) => {
 });
 
 const client = new ApolloClient({
-  uri: "https://diplom-full-stack-v8-hochetsa-plakat-jv1v.vercel.app/graphql",
+  uri: "https://diplom-full-stack-v8-hochetsa-plakat-g4x9.vercel.app/graphql",
   cache: new InMemoryCache(),
   link: from([apolloLogger, authMiddleware, httpLink])
 });
