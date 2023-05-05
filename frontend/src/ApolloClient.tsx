1b6ca77da11f51a7f9d684d458ec0804964d2d35
anton <anton@mail.ru>
2023-05-05 13:15:59 +0300

    jty65hd

--- frontend/src/ApolloClient.tsx
+++ frontend/src/ApolloClient.tsx
@@ -22,13 +22,9 @@ const authMiddleware = new ApolloLink((operation, forward) => {
   operation.setContext(({ headers = {} }) => ({
     headers: {
       ...headers,
-      'Access-Control-Allow-Origin': "*",
-      'Access-Control-Allow-Methods': "GET, POST, PATCH, PUT, DELETE, OPTIONS" ,
-      'Access-Control-Allow-Headers': "Origin, Content-Type, X-Auth-Token",
       'x-access-token': tokens.accessToken,
       'x-refresh-token': tokens.refreshToken
-    },
-    mode: "no-cors"
+    }
   }));
 
   
